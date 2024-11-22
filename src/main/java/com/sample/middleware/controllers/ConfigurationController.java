07e6e83d01a2793c1d523786f90dc741952a470f
Abubakar Amasa <amasaabubaakr@gmail.com>
2024-11-22 00:56:45 +0000

    re-trigger

--- src/main/java/com/sample/middleware/controllers/ConfigurationController.java
+++ src/main/java/com/sample/middleware/controllers/ConfigurationController.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
+//
 @RestController
 @RequestMapping("/api/configuration")
 @AllArgsConstructor
